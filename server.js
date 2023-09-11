52dc6706d0772cd4eb1c5668b6215ca6cde49638
Mostakim Reza Omey <mostakim@Mostakims-MacBook-Air.local>
2023-09-11 17:19:44 +0800

    first

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+}) 
+
+app.get('/', (req, res)=>{
+    const users =[
+        {id:1, name:'Mocha'},
+        {id:2, name:'Cappuccino'},
+        {id:3, name:'Americano'},
+    ]
+    res.json(users);
+})
+
